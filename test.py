6ce98675d532e3c2229e59026f87571d794d3e32
Oleksandr0605 <ivaniul.pn@ucu.edu.ua>
2023-08-13 23:55:39 +0300

    Create test and instruction

---
+++ test.py
@@ -0,0 +1,18 @@
+"""
+Test for python and git
+"""
+
+def hello():
+    """
+    Write hello world in this function
+    """
+    # Write your code here
+
+def main():
+    """
+    Main function
+    """
+    hello()
+
+if __name__ == '__main__':
+    main()
